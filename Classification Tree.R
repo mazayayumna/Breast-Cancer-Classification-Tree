ee9dad1708b474af0da7f893578e549261c862b8
Mazaya Yumna <79080142+mazayayumna@users.noreply.github.com>
2021-07-29 20:46:17 +0700

    Update Classification Tree.R

--- Classification Tree.R
+++ Classification Tree.R
@@ -8,7 +8,7 @@ library(gbm) #gradient boosting
 install.packages("caret")
 library(caret) #tune hyper-parameters
 
-bcancer=read.table("D:\\KULIAHAN\\sem 7\\DSS\\Project 1\\breastcancer.txt",header=T)
+bcancer=read.table("D:\\...\\breastcancer.txt",header=T)
 head(bcancer)
 str(bcancer)
 summary(bcancer)
